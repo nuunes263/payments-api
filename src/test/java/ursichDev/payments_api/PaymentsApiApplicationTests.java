8ae4cc7d96f8c7dbe4ee777ba9bc9185e8428b72
Tiago Ursich <tiagoursich@gmail.com>
2026-07-30 21:50:48 -0300

    first commit

---
+++ src/test/java/ursichDev/payments_api/PaymentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ursichDev.payments_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
